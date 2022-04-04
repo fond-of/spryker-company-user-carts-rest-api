6b5e8efb001ed1829df6f2dcf61b10b5a006b24b
Daniel Rose <daniel-rose@gmx.de>
2022-04-04 08:50:21 +0200

    🧩 Implement glue contracts for updating quote over zed - implement cart updater - add unit tests - ...

--- src/FondOfSpryker/Glue/CompanyUserCartsRestApi/CompanyUserCartsRestApiFactory.php
+++ src/FondOfSpryker/Glue/CompanyUserCartsRestApi/CompanyUserCartsRestApiFactory.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types = 1);
-
 namespace FondOfSpryker\Glue\CompanyUserCartsRestApi;
 
 use FondOfSpryker\Glue\CompanyUserCartsRestApi\Dependency\Client\CompanyUserCartsRestApiToCartClientInterface;
@@ -9,6 +7,8 @@ use FondOfSpryker\Glue\CompanyUserCartsRestApi\Dependency\Client\CompanyUserCart
 use FondOfSpryker\Glue\CompanyUserCartsRestApi\Dependency\Client\CompanyUserCartsRestApiToCompanyUserReferenceClientInterface;
 use FondOfSpryker\Glue\CompanyUserCartsRestApi\Dependency\Client\CompanyUserCartsRestApiToPersistentCartClientInterface;
 use FondOfSpryker\Glue\CompanyUserCartsRestApi\Dependency\Client\CompanyUserCartsRestApiToQuoteClientInterface;
+use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Builder\RestResponseBuilder;
+use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Builder\RestResponseBuilderInterface;
 use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Cart\CartCreator;
 use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Cart\CartCreatorInterface;
 use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Cart\CartDeleter;
@@ -17,12 +17,30 @@ use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Cart\CartOperation;
 use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Cart\CartOperationInterface;
 use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Cart\CartReader;
 use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Cart\CartReaderInterface;
-use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Cart\CartUpdater;
-use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Cart\CartUpdaterInterface;
+use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Expander\RestCartItemExpander;
+use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Expander\RestCartItemExpanderInterface;
+use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Filter\CompanyUserReferenceFilter;
+use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Filter\CompanyUserReferenceFilterInterface;
+use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Filter\CustomerReferenceFilter;
+use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Filter\CustomerReferenceFilterInterface;
+use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Filter\IdCartFilter;
+use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Filter\IdCartFilterInterface;
 use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Mapper\CartItemsResourceMapper;
 use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Mapper\CartItemsResourceMapperInterface;
 use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Mapper\CartsResourceMapper;
 use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Mapper\CartsResourceMapperInterface;
+use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Mapper\RestCartsAttributesMapper;
+use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Mapper\RestCartsAttributesMapperInterface;
+use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Mapper\RestCartsDiscountsMapper;
+use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Mapper\RestCartsDiscountsMapperInterface;
+use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Mapper\RestCartsTotalsMapper;
+use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Mapper\RestCartsTotalsMapperInterface;
+use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Mapper\RestCompanyUserCartsRequestMapper;
+use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Mapper\RestCompanyUserCartsRequestMapperInterface;
+use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Mapper\RestItemsAttributesMapper;
+use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Mapper\RestItemsAttributesMapperInterface;
+use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Updater\CartUpdater;
+use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Updater\CartUpdaterInterface;
 use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Validation\RestApiError;
 use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Validation\RestApiErrorInterface;
 use Spryker\Glue\CartsRestApi\CartsRestApiConfig;
@@ -71,21 +89,6 @@ class CompanyUserCartsRestApiFactory extends AbstractFactory
             $this->getResourceBuilder(),
             $this->getCompanyUserReferenceClient(),
             $this->createRestApiError(),
-        );
-    }
-
-    /**
-     * @return \FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Cart\CartUpdaterInterface
-     */
-    public function createCartUpdater(): CartUpdaterInterface
-    {
-        return new CartUpdater(
-            $this->createCartReader(),
-            $this->createCartOperation(),
-            $this->getPersistentCartClient(),
-            $this->createCartsResourceMapper(),
-            $this->createRestApiError(),
-            $this->getResourceBuilder(),
         );
     }
 
@@ -176,6 +179,112 @@ class CompanyUserCartsRestApiFactory extends AbstractFactory
             $this->createCartItemsResourceMapper(),
             $this->getRestCartItemExpanderPlugins(),
         );
+    }
+
+    /**
+     * @return \FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Updater\CartUpdater
+     */
+    public function createCartUpdater(): CartUpdaterInterface
+    {
+        return new CartUpdater(
+            $this->createRestCompanyUserCartsRequestMapper(),
+            $this->createRestCartItemExpander(),
+            $this->createRestResponseBuilder(),
+            $this->getClient(),
+        );
+    }
+
+    /**
+     * @return \FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Mapper\RestCompanyUserCartsRequestMapperInterface
+     */
+    protected function createRestCompanyUserCartsRequestMapper(): RestCompanyUserCartsRequestMapperInterface
+    {
+        return new RestCompanyUserCartsRequestMapper(
+            $this->createIdCartFilter(),
+            $this->createCompanyUserReferenceFilter(),
+            $this->createCustomerReferenceFilter(),
+        );
+    }
+
+    /**
+     * @return \FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Filter\IdCartFilterInterface
+     */
+    protected function createIdCartFilter(): IdCartFilterInterface
+    {
+        return new IdCartFilter();
+    }
+
+    /**
+     * @return \FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Filter\CompanyUserReferenceFilterInterface
+     */
+    protected function createCompanyUserReferenceFilter(): CompanyUserReferenceFilterInterface
+    {
+        return new CompanyUserReferenceFilter();
+    }
+
+    /**
+     * @return \FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Filter\CustomerReferenceFilterInterface
+     */
+    protected function createCustomerReferenceFilter(): CustomerReferenceFilterInterface
+    {
+        return new CustomerReferenceFilter();
+    }
+
+    /**
+     * @return \FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Expander\RestCartItemExpanderInterface
+     */
+    protected function createRestCartItemExpander(): RestCartItemExpanderInterface
+    {
+        return new RestCartItemExpander(
+            $this->getRestCartItemExpanderPlugins(),
+        );
+    }
+
+    /**
+     * @return \FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Builder\RestResponseBuilderInterface
+     */
+    protected function createRestResponseBuilder(): RestResponseBuilderInterface
+    {
+        return new RestResponseBuilder(
+            $this->createRestCartsAttributesMapper(),
+            $this->createRestItemsAttributesMapper(),
+            $this->getResourceBuilder(),
+        );
+    }
+
+    /**
+     * @return \FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Mapper\RestCartsAttributesMapperInterface
+     */
+    protected function createRestCartsAttributesMapper(): RestCartsAttributesMapperInterface
+    {
+        return new RestCartsAttributesMapper(
+            $this->createRestCartsDiscountsMapper(),
+            $this->createRestCartsTotalsMapper(),
+        );
+    }
+
+    /**
+     * @return \FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Mapper\RestCartsDiscountsMapperInterface
+     */
+    protected function createRestCartsDiscountsMapper(): RestCartsDiscountsMapperInterface
+    {
+        return new RestCartsDiscountsMapper();
+    }
+
+    /**
+     * @return \FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Mapper\RestCartsTotalsMapperInterface
+     */
+    protected function createRestCartsTotalsMapper(): RestCartsTotalsMapperInterface
+    {
+        return new RestCartsTotalsMapper();
+    }
+
+    /**
+     * @return \FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Mapper\RestItemsAttributesMapperInterface
+     */
+    protected function createRestItemsAttributesMapper(): RestItemsAttributesMapperInterface
+    {
+        return new RestItemsAttributesMapper();
     }
 
     /**
---
+++ tests/FondOfSpryker/Glue/CompanyUserCartsRestApi/CompanyUserCartsRestApiFactoryTest.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace FondOfSpryker\Glue\CompanyUserCartsRestApi;
+
+use Codeception\Test\Unit;
+use FondOfSpryker\Client\CompanyUserCartsRestApi\CompanyUserCartsRestApiClient;
+use FondOfSpryker\Glue\CompanyUserCartsRestApi\Processor\Updater\CartUpdater;
+use Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface;
+use Spryker\Glue\Kernel\Container;
+
+class CompanyUserCartsRestApiFactoryTest extends Unit
+{
+    /**
+     * @var \FondOfSpryker\Client\CompanyUserCartsRestApi\CompanyUserCartsRestApiClient|\PHPUnit\Framework\MockObject\MockObject
+     */
+    protected $clientMock;
+
+    /**
+     * @var \FondOfSpryker\Glue\CompanyUserCartsRestApi\CompanyUserCartsRestApiConfig|\PHPUnit\Framework\MockObject\MockObject
+     */
+    protected $configMock;
+
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject|\Spryker\Glue\Kernel\Container
+     */
+    protected $containerMock;
+
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject|\Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface
+     */
+    protected $restResourceBuilderMock;
+
+    /**
+     * @var \FondOfSpryker\Glue\CompanyUserCartsRestApi\CompanyUserCartsRestApiFactory
+     */
+    protected $factory;
+
+    /**
+     * @return void
+     */
+    protected function _before(): void
+    {
+        parent::_before();
+
+        $this->clientMock = $this->getMockBuilder(CompanyUserCartsRestApiClient::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->configMock = $this->getMockBuilder(CompanyUserCartsRestApiConfig::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->containerMock = $this->getMockBuilder(Container::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->restResourceBuilderMock = $this->getMockBuilder(RestResourceBuilderInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->factory = new class ($this->restResourceBuilderMock) extends CompanyUserCartsRestApiFactory {
+            /**
+             * @var \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface
+             */
+            protected $restResourceBuilder;
+
+            /**
+             * @param \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface $restResourceBuilder
+             */
+            public function __construct(RestResourceBuilderInterface $restResourceBuilder)
+            {
+                $this->restResourceBuilder = $restResourceBuilder;
+            }
+
+            /**
+             * @return \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface
+             */
+            public function getResourceBuilder(): RestResourceBuilderInterface
+            {
+                return $this->restResourceBuilder;
+            }
+        };
+
+        $this->factory->setClient($this->clientMock);
+        $this->factory->setConfig($this->configMock);
+        $this->factory->setContainer($this->containerMock);
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreateCartUpdater(): void
+    {
+        $this->containerMock->expects(static::atLeastOnce())
+            ->method('has')
+            ->with(CompanyUserCartsRestApiDependencyProvider::PLUGINS_REST_CART_ITEM_EXPANDER)
+            ->willReturn(true);
+
+        $this->containerMock->expects(static::atLeastOnce())
+            ->method('get')
+            ->with(CompanyUserCartsRestApiDependencyProvider::PLUGINS_REST_CART_ITEM_EXPANDER)
+            ->willReturn([]);
+
+        static::assertInstanceOf(
+            CartUpdater::class,
+            $this->factory->createCartUpdater()
+        );
+    }
+}
